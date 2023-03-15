2b440eecda07536e05f86e1992c8a2c17626a690
Derrick U. Onyeani-Nwosu <uka.onyeani@gmail.com>
2023-03-15 11:07:02 -0400

    updated the code to do a much stricter checking (to eliminate the extra fizz or Buzz after the FizzBuzz) and to also prompt for user input.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,23 +1,18 @@
-# Given number n.
-# Prompt user for input
-# Looping from 0-n
-# if number is divisible by 3 print "Fizz"
-# if number is divisible by 5 print "Buzz"
-# if number is divisible by 3 and 5 print "FizzBuzz"
-# if number is divisible by 3 or 5 return Number
+# Prompt the user for a range of numbers to evaluate
+start_num = int(input("Enter the starting number: "))
+end_num = int(input("Enter the ending number: "))
 
-n = 100
-
-for num in range(1, (n+1)):
+# Looping from start_num - end_num
+for num in range(start_num, end_num):
     # if number is divisible by 3 and 5 print "FizzBuzz"
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
     # if number is divisible by 3 print "Fizz"
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 != 0:
         print("Fizz")
     # if number is divisible by 5 print "Buzz"
-    elif num % 5 == 0:
+    elif num % 5 == 0 and num % 3 != 0:
         print("Buzz")
-    # if number is divisible by 3 or 5 return Number
-    else:
+    # if number is not divisible by 3 or 5 print "Buzz"
+    elif num % 5 != 0 or num % 3 != 0:
         print(num)
